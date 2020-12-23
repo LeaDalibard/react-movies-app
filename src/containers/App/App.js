db5d93499670d94d1f10ab855a6de43c33ec624d
Lea Dalibard <leadalibard@gmail.com>
2020-12-23 12:19:57 +0100

    Changing App.js to a class extending Component

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,21 +1,36 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import Movie from '../../components/Movie'
 import data from '../../constants/data'
 
 import './App.css'
 
-const App = () => (
-    <div className="App">
+class App extends Component {
 
-        {data.map(({imdbID, ...movie}) => (
-            <Movie
-                key={imdbID}
-                {...movie}
-            />
-        ))}
-    </div>
-//Use spread operator '...movie' instead of 'Title, Year, Poster' because movies and data have the same properties/keys
-)
+    render = () => (
+        <div className="App">
+            {data.map(({imdbID, ...movie}) => (
+                <Movie
+                    key={imdbID}
+                    {...movie}
+                />
+                ))}
+        </div>
+    )
+}
 
 export default App;
+
+// Former code :
+//const App = () => (
+//     <div className="App">
+//
+//         {data.map(({imdbID, ...movie}) => (
+//             <Movie
+//                 key={imdbID}
+//                 {...movie}
+//             />
+//         ))}
+//     </div>
+// //Use spread operator '...movie' instead of 'Title, Year, Poster' because movies and data have the same properties/keys
+// )
