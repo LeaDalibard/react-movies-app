08b8d7af8e4bd477f8bee2b1d96e52c0c7316dfb
Lea Dalibard <leadalibard@gmail.com>
2020-12-23 10:50:57 +0100

    Adding prop-Type to hint properties

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,5 +1,6 @@
 import React from 'react'
 //On importe React pour que JSX soit interprété
+import PropTypes from 'prop-types'
 import './Movie.css'
 
 const Movie=({Title, Year, Poster})=>
@@ -13,11 +14,19 @@ const Movie=({Title, Year, Poster})=>
             </div>
         </div>
     )
+// for images dont put "" after src, directly expression
 
+Movie.propTypes={
+    Title: PropTypes.string.isRequired,
+    Year: PropTypes.oneOfType([PropTypes.string,PropTypes.number]),
+    Poster:PropTypes.string,
+}
 Movie.defaultProps = {
     Title: "WITHOUT TITLE",
     Year: 0,
     Poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoCzq9xjEDLMt0eFPm5RP_-kTFYleKW3iheQ&usqp=CAU',
 }
-// for images dont put "" after src, directly expression
+
+
+
 export default Movie
