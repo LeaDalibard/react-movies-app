cc369698354db00293fbe6c27f0c5073bfac8185
Lea Dalibard <leadalibard@gmail.com>
2020-12-23 12:04:45 +0100

    Imported library React-feather, made a class Movie extending component with static props and toggle method for favorite movie, add a state Favorite and linked it to a button with icons with the toggle method

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,32 +1,70 @@
-import React from 'react'
+import React, {Component} from 'react'
 //On importe React pour que JSX soit interprété
 import PropTypes from 'prop-types'
+import * as Icon from 'react-feather'
 import './Movie.css'
 
-const Movie=({Title, Year, Poster})=>
-    (
-        <div className="Movie">
+class Movie extends Component {
+    static propTypes = {
+        Title: PropTypes.string.isRequired,
+        Year: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        Poster: PropTypes.string,
+    }
+    static defaultProps = {
+        Title: "WITHOUT TITLE",
+        Year: 0,
+        Poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoCzq9xjEDLMt0eFPm5RP_-kTFYleKW3iheQ&usqp=CAU',
+    }
 
-            <img className="poster" src={Poster} alt="poster"/>
-            <div className="meta">
-                <h3 className="title">{Title}</h3>
-                <span className="year">{Year}</span>
+    state = {isFavorite: false}
+
+    _toggleFavorite = () => this.setState({isFavorite: !this.state.isFavorite})
+
+    render = () => {
+
+        const {Title, Year, Poster} = this.props
+        const {isFavorite} = this.state
+
+        return (
+
+            <div className="Movie">
+                <img className="poster" src={Poster} alt="poster"/>
+                <div className="meta">
+                    <button className="favorite" type="button" onClick={this._toggleFavorite}>{isFavorite ? <Icon.CheckCircle/> :<Icon.PlusCircle/> }</button>
+
+                    <h3 className="title">{Title}</h3>
+                    <span className="year">{Year}</span>
+                </div>
             </div>
-        </div>
-    )
+
+        )
+
+    }
+}
+
+
 // for images dont put "" after src, directly expression
-
-Movie.propTypes={
-    Title: PropTypes.string.isRequired,
-    Year: PropTypes.oneOfType([PropTypes.string,PropTypes.number]),
-    Poster:PropTypes.string,
-}
-Movie.defaultProps = {
-    Title: "WITHOUT TITLE",
-    Year: 0,
-    Poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoCzq9xjEDLMt0eFPm5RP_-kTFYleKW3iheQ&usqp=CAU',
-}
-
-
+//Former code without Class :
+// Movie.propTypes={
+//     Title: PropTypes.string.isRequired,
+//     Year: PropTypes.oneOfType([PropTypes.string,PropTypes.number]),
+//     Poster:PropTypes.string,
+// }
+// Movie.defaultProps = {
+//     Title: "WITHOUT TITLE",
+//     Year: 0,
+//     Poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoCzq9xjEDLMt0eFPm5RP_-kTFYleKW3iheQ&usqp=CAU',
+// }
+//const Movie = ({Title, Year, Poster}) =>
+//     (
+//         <div className="Movie">
+//
+//             <img className="poster" src={Poster} alt="poster"/>
+//             <div className="meta">
+//                 <h3 className="title">{Title}</h3>
+//                 <span className="year">{Year}</span>
+//             </div>
+//         </div>
+//     )
 
 export default Movie
