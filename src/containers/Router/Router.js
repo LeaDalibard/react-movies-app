ab42c79e31fa6adfab881e40dd33895d21fc3bb4
Lea Dalibard <leadalibard@gmail.com>
2020-12-24 10:42:11 +0100

    Adding first Router Search + Browser router

--- src/containers/Router/Router.js
+++ src/containers/Router/Router.js
@@ -1,2 +1,18 @@
 import React from 'react'
-import {Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import Search from "../../containers/Search";
+//import Details from "../../containers/Details";
+
+const Router = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={Search}/>
+            {/*<Route path='/movie/:imdbID' component={Details}/>*/}
+        </Switch>
+    </BrowserRouter>
+)
+
+export default Router
+
+// Switch de route en fonction de l'url
