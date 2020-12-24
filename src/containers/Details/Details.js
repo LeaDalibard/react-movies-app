1b168a59cd04189daa8aa978b95d2a8905abb22c
Lea Dalibard <leadalibard@gmail.com>
2020-12-24 11:18:40 +0100

    Making a composant Details

---
+++ src/containers/Details/Details.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+import './Details.css'
+
+class Details extends Component {
+
+    static propTypes={}
+
+    render =()=>"details"
+}
+
+export default Details
